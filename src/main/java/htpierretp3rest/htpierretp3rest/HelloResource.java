39a98c729819583565f391e81e0cef07f29b75eb
Dawens Pierre <pdawens78@gmail.com>
2026-05-13 02:33:01 -0400

    Test avec Parametre Personne

--- src/main/java/htpierretp3rest/htpierretp3rest/HelloResource.java
+++ src/main/java/htpierretp3rest/htpierretp3rest/HelloResource.java
@@ -2,13 +2,17 @@ package htpierretp3rest.htpierretp3rest;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello-world")
 public class HelloResource {
+
     @GET
-    @Produces("text/plain")
-    public String hello() {
-        return "Hello, World!";
+    @Path("personnes/{nom}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello(@PathParam("nom") String nom) {
+        return "Hello, " + nom;
     }
 }
